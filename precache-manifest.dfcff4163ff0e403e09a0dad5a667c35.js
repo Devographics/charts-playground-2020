a48b716f747638a3a4c1637d223f8ada32e5e828
plouc <benitteraphael@gmail.com>
2020-10-25 10:23:15 +0900

    Updates

---
+++ precache-manifest.dfcff4163ff0e403e09a0dad5a667c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a994c0620e84c53e43a5ff76bf77e80d",
+    "url": "/charts-playground-2020/index.html"
+  },
+  {
+    "revision": "82cedc584a9e49944df5",
+    "url": "/charts-playground-2020/static/css/main.78eab835.chunk.css"
+  },
+  {
+    "revision": "bdb4b4b093eed734d6cf",
+    "url": "/charts-playground-2020/static/js/2.8b369852.chunk.js"
+  },
+  {
+    "revision": "b72db6fcf28c4a8a27532185c9ee63b6",
+    "url": "/charts-playground-2020/static/js/2.8b369852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82cedc584a9e49944df5",
+    "url": "/charts-playground-2020/static/js/main.51fd7c8a.chunk.js"
+  },
+  {
+    "revision": "f42b946aa460248de94a",
+    "url": "/charts-playground-2020/static/js/runtime-main.097ab8cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa47b6909d84b8322f209a51b3c54376.js"
+  "/charts-playground-2020/precache-manifest.dfcff4163ff0e403e09a0dad5a667c35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/charts-playground-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
